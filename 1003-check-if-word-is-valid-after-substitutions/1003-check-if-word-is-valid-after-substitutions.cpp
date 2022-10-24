e55a080e5f99ec39a32a76bf8628a9b0a61568b3
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-10-24 23:24:13 +0530

    Time: 72 ms (19.43%), Space: 8.5 MB (24.34%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,39 +1,7 @@
 class Solution {
 public:
     bool isValid(string s) {
-//         stack<char> st;
-        
-//         st.push(s[0]);
-        
-//         for(int i=1; i<s.length(); i++){
-//             if(st.top() == 'c'){
-//                 st.pop();
-//             }
-//             if(!st.empty()){
-//                 char last = st.top();
-//                 if(last == 'a'){
-//                     if(s[i] == 'b'){
-//                         st.pop();
-//                     }
-//                 } else if(last == 'b'){
-//                     if(s[i] == 'c'){
-//                         st.pop();
-//                     }
-//                 } 
-//             }
-            
-            
-//             st.push(s[i]);
-//         }
-        
-//         if(st.size() == 1 && st.top() == 'c'){
-//             return true;
-//         }
-        
-//         return false;
-        
         stack<char>st;
-        // set<string>see;
         map<string, int> mp;
         int res = 0;
         for(auto it: s){
