90fb60153dfbb25bfffbfacc063ded6081be1ec4
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-11-29 00:16:12 +0530

    Time: 122 ms (81.74%), Space: 43.8 MB (35.82%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -5,7 +5,7 @@ public:
         
         int sum = 0;
         int count = 0;
-        unordered_map<int,int> mp;
+        map<int,int> mp;
         
         for(int i=0; i<nums.size(); i++){
             sum += nums[i];
