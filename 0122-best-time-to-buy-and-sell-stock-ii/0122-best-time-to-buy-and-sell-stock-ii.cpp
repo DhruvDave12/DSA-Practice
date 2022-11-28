e0974d69cb07b50dd6349f9ba8dcd069e833789f
Dhruv Dave <78278281+DhruvDave12@users.noreply.github.com>
2022-11-29 01:28:06 +0530

    Time: 19 ms (33.73%), Space: 13.1 MB (30.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,15 +3,12 @@ public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
         int curr = prices[0];
-        // int limit = 2;
+
         
         for(int i=1;i<prices.size(); i++){
-            // if(limit == 0) break;
             if(prices[i] <= curr){
-                cout<<curr<<"\n";
                 curr = prices[i];
             }    
-            
             else if(prices[i] > curr){
                 int sellDay = INT_MIN;
                 while(i<prices.size() && prices[i] >= sellDay){
@@ -22,10 +19,8 @@ public:
                 if(i < prices.size()){
                     x = prices[i];
                 }
-                // cout<<sellDay<<" "<<curr<<"\n";
                 profit += sellDay-curr;
                 curr = x;
-                // limit--;
                 i--;
             }
         }
